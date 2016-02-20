ff649efcfa2820f8482b09ed4da77c85a0e91d8c
Tony Lane <CodeGuro@gmail.com>
2016-02-20 01:44:59 -0500

    added commenting/docs

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -4,6 +4,9 @@
 #include <limits.h>
 #include <mpi.h>
 
+// one of the few cases where #defines are OK
+#define TINY_SIZE 128
+
 // sorting algorithm of complexity O( nlogn )
 int * mergesort( int *arr, int n )
 {
@@ -93,15 +96,17 @@ void write_outfile( char const *fname, int *buff, int n )
 
 int main( int argc, char **argv )
 {
+	// initialize MPI
 	MPI_Init( &argc, &argv );
 
+	// get MPI info
 	int rank;
 	int comm_size;
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &comm_size );
 
+	// declarations
 	printf( "this rank is %i and the size is %i\n", rank, comm_size );
-	int *buff = NULL;
 	int arrsize = -1;
 	int *rec_buff = NULL;
 	int rec_buff_size = -1;
@@ -113,51 +118,59 @@ int main( int argc, char **argv )
 		if( hfile == NULL )
 			return printf( "no such file %s", "%s" );
 
-		char buffer[ 1024 ] = { 0 };
-
+		// read the file for element count and cap the per-proc buffer size
+		char buffer[ TINY_SIZE ] = { 0 };
 		fgets( buffer, sizeof( buffer ), hfile );
 		arrsize = atoi( buffer );
 		rec_buff_size = ( arrsize / comm_size ) + ( ( arrsize % comm_size ) ? 1 : 0 );
 
-		buff = malloc( rec_buff_size * comm_size * sizeof( int ) );
 		// map the buffer with the integers
+		int *buff = malloc( rec_buff_size * comm_size * sizeof( int ) );
 		for( int it = 0; it < arrsize; ++it )
 		{
-			char mini_buff[ 128 ] = { 0 };
+			memset( buffer, '\0', sizeof( buffer ) );
 			char c;
-			while( isspace( ( c = fgetc( hfile ) ) ) );
-			for( int it2 = 0; it2 < sizeof( mini_buff ) && !isspace( c ); c = fgetc( hfile ) )
-				mini_buff[ it2++ ] = c;
 
-			buff[ it ] = atoi( mini_buff );
+			while( isspace( ( c = fgetc( hfile ) ) ) ); // skip the spaces
+
+			for( int it2 = 0; it2 < sizeof( buffer ) && !isspace( c ); c = fgetc( hfile ) )
+				buffer[ it2++ ] = c;
+
+			buff[ it ] = atoi( buffer );
 		}
 
-		// fill the rest with max_int (to be removed later)
+		// fill the cap with MAX_INT (to be ignored later)
 		for( int it = arrsize; it < rec_buff_size * comm_size; ++it )
 			buff[ it ] = INT_MAX;
 		
+		// scatter the array across other processes 
 		rec_buff = malloc( rec_buff_size * sizeof( int ) );
-		MPI_Bcast( &rec_buff_size, 1, MPI_INT, 0, MPI_COMM_WORLD );
+		MPI_Bcast( &rec_buff_size, 1, MPI_INT, 0, MPI_COMM_WORLD ); // broadcast the data size
 		MPI_Scatter( buff, rec_buff_size, MPI_INT, rec_buff, rec_buff_size, MPI_INT, 0, MPI_COMM_WORLD );
 		printf( "parent broadcast sent\n" );
 		printf( "child rank %i recieved the buffer size. It is %i\n", rank, rec_buff_size );
 
-		// clean up
+		// clean up 
 		free( buff );
 		fclose( hfile );
 	}
 	else // child
 	{
+		// recieve the data size
 		printf( "Entering child rank %i\n", rank );
 		MPI_Bcast( &rec_buff_size, 1, MPI_INT, 0, MPI_COMM_WORLD );
 		printf( "child rank %i recieved the buffer size. It is %i\n", rank, rec_buff_size );
 		rec_buff = malloc( rec_buff_size * sizeof( int ) );
+
+		// recieve the data
 		MPI_Scatter( NULL, 0, MPI_DATATYPE_NULL, rec_buff, rec_buff_size, MPI_INT, 0, MPI_COMM_WORLD );
 		printf( "child rank %i recieved the data\n", rank );
 	}
 
+	// finally, sort the data
 	mergesort( rec_buff, rec_buff_size );
 
+	// begin merging the data
 	for( int step = 1; step < comm_size; step *= 2 )
 	{
 		if( rank % (2 * step) )
@@ -169,6 +182,7 @@ int main( int argc, char **argv )
 		{
 			if( rank + step < comm_size )
 			{
+				// allocate a second buffer to recieve data, and double the buffer size
 				printf( "rank %i is merging from rank %i\n", rank, rank + step );
 				int *second_buff = malloc( rec_buff_size * sizeof( int ) );
 				int *new_rec_buff = malloc( rec_buff_size * 2 * sizeof( int ) );
@@ -182,11 +196,12 @@ int main( int argc, char **argv )
 		}
 	}
 
+	// create and write the output file
 	if( rank == 0 )
 		write_outfile( argv[ 2 ], rec_buff, arrsize );
 
+	// clean up
 	free( rec_buff );
-
 	MPI_Finalize();
 
 	return 0;
